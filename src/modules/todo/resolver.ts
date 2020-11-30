841debfdfd50659b91684e8a962e2be0a0328bf1
Karthik Kumar <kumarkarthik.g@gmail.com>
2020-11-29 21:14:46 -0600

    First commit

---
+++ src/modules/todo/resolver.ts
@@ -0,0 +1,33 @@
+import { Resolver, Arg, Query, Mutation, ID } from "type-graphql";
+import { Service } from "typedi";
+import { ObjectId } from "mongodb";
+
+import { Todo } from "../../entities";
+import TodoService from "./service";
+import { NewTodoInput } from "./input";
+import { ObjectIdScalar } from "../../utils";
+
+/*
+  IMPORTANT: Your business logic must be in the service!
+*/
+
+@Service() // Dependencies injection
+@Resolver((of) => Todo)
+export default class TodoResolver {
+  constructor(private readonly todoService: TodoService) {}
+
+  @Query((returns) => Todo, {nullable: true})
+  async getTodo(@Arg("id", type => ObjectIdScalar) id: ObjectId) {
+    const todo = await this.todoService.getById(id);
+
+    return todo;
+  }
+
+  @Mutation((returns) => Todo)
+  async createTodo(
+    @Arg("createTodoData") createTodoData: NewTodoInput
+  ): Promise<Todo> {
+    const todo = await this.todoService.addTodo(createTodoData);
+    return todo;
+  }
+}
